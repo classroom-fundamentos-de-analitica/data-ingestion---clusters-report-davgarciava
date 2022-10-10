6c85fc185bf4b4d174fa9c0212d7c00b39659873
David Mateo García Vallejo <80338757+davgarciava@users.noreply.github.com>
2022-10-10 17:59:11 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,38 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
+  with open('clusters_report.txt') as report:
+    filas = report.readlines()
 
-    #
-    # Inserte su código aquí
-    #
+  filas = filas[4:]
 
-    return df
+  clusters = []
+  cluster = [0, 0, 0, '']
+
+  for fila in filas:
+    if re.match('^ +[0-9]+ +', fila):
+      numero, cantidad, porcentaje, *palabras = fila.split()
+      
+      cluster[0] = int(numero)
+      cluster[1] = int(cantidad)
+      cluster[2] = float(porcentaje.replace(',','.'))
+
+      palabras.pop(0)
+      palabras = ' '.join(palabras)
+      cluster[3] += palabras
+
+    elif re.match('^ +[a-z]', fila):
+      palabras = fila.split()
+      palabras = ' '.join(palabras)
+      cluster[3] += ' ' + palabras
+
+    elif re.match('^\n', fila) or re.match('^ +$', fila):
+      cluster[3] = cluster[3].replace('.', '')
+      clusters.append(cluster)
+      cluster = [0, 0, 0, '']
+
+  df = pd.DataFrame (clusters, columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+  return df
